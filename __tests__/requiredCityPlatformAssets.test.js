f3d589ba8cc598651f6254127f3683c5c4af8944
Simon Dalmasso <simondalmasso44@gmail.com>
2026-07-21 12:02:46 -0300

    fix(test): permit CommonJS imports in asset contract tests

--- __tests__/requiredCityPlatformAssets.test.js
+++ __tests__/requiredCityPlatformAssets.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 const test = require('node:test');
 const assert = require('node:assert/strict');
 const { readFileSync } = require('node:fs');
